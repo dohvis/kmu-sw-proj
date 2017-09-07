cf155a4224e86d6e6e4fc1177236c5af6fd99fa1
Dohyeon Kim <nero.union12@gmail.com>
2017-09-07 11:10:23 +0900

    Update result code

--- 20171597-김영훈-assignment2.py
+++ 20171597-김영훈-assignment2.py
@@ -1,21 +1,24 @@
 def factorial(num):
     res = 1
-    for i in range(1, num+1):
-        res *= i
+    while num > 1:
+        res *= num
+        num -= 1
     return res
+
+number = 0
 
 while True:
     try:
-        getNum = int(input("Enter a number: "))
+        number = int(input("Enter a number: "))
     except ValueError:
         print("Please enter only a integer !!")
     else:
-        if getNum == -1:
+        if number == -1:
             break
-        elif getNum == 0:
+        elif number == 0:
             print("0! = 1")
-        elif getNum < -1:
+        elif number < -1:
             print("Do not enter the negative number except -1")
         else:
-            result = factorial(getNum)
-            print("%d! = %d" %(getNum,result))
+            result = factorial(number)
+            print("%d! = %d" %(number,result))
