c9564f2148e25e49c506181437a997277998b710
Dohyeon Kim <nero.union12@gmail.com>
2017-09-26 11:07:45 +0900

    Finished

--- week4/20171587-김도현.py
+++ week4/20171587-김도현.py
@@ -1,9 +1,17 @@
-def f(num):
-    return (num * f(num-1)) if num > 1 else 1
+def factorial(num):
+    return (num * factorial(num-1)) if num > 1 else 1
 
-def c(n, r):
-    # return f(n) / (f(n) * (n-r))
-    return  1 if n == r or r == 0 else c(n-1, r-1) + c(n-1, r)
+def combination(n, r):
+    # return factorial(n) / (factorial(n) * (n-r))
+    if not isinstance(n, int) or not isinstance(r, int):
+        raise ValueError('Not int')
+    if (n < r):
+        raise ValueError('n < r')
+    if n < 1 or r < 0:
+        print(n, r)
+        raise ValueError('{n} < 1,  {r} < 0'.format(n=n, r=r))
+    r = min(r, n - r)
+    return 1 if n == r or r == 0 else combination(n-1, r-1) + combination(n-1, r)
 
-print(c(10, 3))
+print(combination(3, 2))
 
