c1a82f66890bb0091847f4c07ded11da04ed5fe4
Namjae Kim <kimnj3050@kookmin.ac.kr>
2017-10-10 11:13:31 +0900

    Namjae

--- week5/김남재.py
+++ week5/김남재.py
@@ -1,8 +1,34 @@
-def fibo(n):
-    fibolist = [0, 1]
-    for i in range(2, n+1):
-        fibolist.append(fibolist[i-1] + fibolist[i-2])
-    return fibolist[n]
+import time
 
-for i in range(0, 100):
-    print(fibo(i))
+
+def fibo_list(n):
+    fibolist = [0, 1]
+    for i in range(2, n + 1):
+        fibolist.append(fibolist[i - 1] + fibolist[i - 2])
+    return fibolist[n]
+def fibo_value(n):
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
+def fibo_rec(n):
+    if n<=1:
+        return n
+    return fiborec(n - 1) + fiborec(n - 2)
+def profile(func, nbr):
+    ts = time.time()
+    fibonum = func(nbr)
+    ts = time.time() - ts
+    print("%s(%d)=%d, time %.6f" % (func.__name__, nbr, fibonum, ts))
+
+while True:
+    try:
+        nbr = int(input("Enter a number: "))
+    except ValueError:
+        print("Only int")
+        continue
+    if nbr == -1:
+        break
+    profile(fibo_list, nbr)
+    profile(fibo_value, nbr)
+    profile(fibo_rec, nbr)
